eb38267f1f9381a95b25fcbcd970f90c9d7d6fd7
pabhoz <pabhoz@gmail.com>
2017-01-31 16:29:26 -0500

    removida la credencial de comparación

--- magia/views/modules/auth.php
+++ magia/views/modules/auth.php
@@ -1,5 +1,5 @@
 <?php
-$userId = Session::get("uid");//Replace for an User BL method
+$userId = 1;//Session::get("uid");//Replace for an User BL method
 if(empty($userId)){
   header("Location:".URL."Login");
 }
