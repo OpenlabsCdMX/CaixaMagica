67564115b38bae03ca5eb1b696aaa4674c2c0463
pabhoz <pabhoz@gmail.com>
2017-07-17 02:09:51 -0500

    Hotfix - Removed unexpected print "some" at Model.php

--- magia/libs/Model.php
+++ magia/libs/Model.php
@@ -303,7 +303,7 @@ class Model {
 
     public static function getBy($field, $data, $all = false) {
         self::setTableForStaticCall();
-        print_r("some");
+        //print_r("some");
         $whereData = self::where($field, $data);
         if($all){
             $data = array_shift($whereData);
